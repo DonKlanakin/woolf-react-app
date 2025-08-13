b9c9f239fbe8a0617202a53627b9b2409d6c9d62
DK <don@promptnow.com>
2025-08-13 09:15:44 +0700

    Update ItemCard info.

--- src/components/item-card/ItemCard.jsx
+++ src/components/item-card/ItemCard.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const ItemCard = ({item}) =>
 {
+	const BASE_URL = `https://image.tmdb.org/t/p/w500`;
+	let thumbnailPath = item.poster_path;
+
 	return (
-		<div>
+		<div className="item-card">
+			<img className="card-img-top" src={thumbnailPath ? `${BASE_URL}${thumbnailPath}` : "/images/poster-not-available.png"} />
 			<li className="text-white" key={item.id}>{item.title}</li>
 		</div>
 	);
