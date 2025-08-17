401de3a3f51aae8ea5a6115f66c65f71d9f3493b
DK <don@promptnow.com>
2025-08-17 20:10:31 +0700

    Update SearchBar: Add clear search functionality

--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 const SearchBar = ({searchTerm, setSearchTerm}) =>
 {
+	const clearSearchBar = () => setSearchTerm("");
+
 	return (
 		<div className="search">
 			<figure>
-				<img src="./images/search.svg" alt="Search Icon"/>
+				<img src="./images/search.svg" alt="Search Icon" className="search-icon" />
 				<input type="text"
-							 placeholder="Search through the MTG Multiverse.."
+							 placeholder={`Search through the MTG Multiverse..   (try "Gandalf The Gray" or "Deadpool"!)`}
 							 value={searchTerm}
 							 onChange={(event) => setSearchTerm(event.target.value)}/>
+				<img src="./images/cancel.svg" alt="Search Icon" className="clear-icon opacity-80 cursor-pointer" onClick={clearSearchBar}/>
 			</figure>
 		</div>
 	);
