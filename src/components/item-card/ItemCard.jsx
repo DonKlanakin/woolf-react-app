cda681ead16cc5227bdeecf95bacff14a5f0f9a3
DK <don@promptnow.com>
2025-08-17 08:20:10 +0700

    Add info to card: released date

--- src/components/item-card/ItemCard.jsx
+++ src/components/item-card/ItemCard.jsx
@@ -12,11 +12,16 @@ const ItemCard = ({item}) =>
 				<img src={thumbnailPath ? `${thumbnailPath}` : "/images/poster-not-available.png"} alt="artwork"/>
 				<h3 className="mt-4">{item.name}</h3>
 
-				<div className="content">
+				<div className="content mx-auto justify-between">
 					<div className="rating">
 						<img src="images/star.svg" alt="star"/>
 						<p className="text-sm text-light-100">{item.rarity}</p>
 					</div>
+
+					<div className="year">
+						<p>{item.released_at}</p>
+					</div>
+
 				</div>
 			</div>
 		);
